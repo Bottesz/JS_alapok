b3232eb5aa4583affdbeeb053fa4b5ddad5d22fc
AlexandraLukacs <lszandika15@gmail.com>
2024-03-06 13:46:43 +0100

    gyakorlas

--- script.js
+++ script.js
@@ -1,15 +1,70 @@
 window.addEventListener("load", function () {
-    elemekElerese1();
-    elemekElerese2();
-    elemekElerese3();
+  elemekElerese1();
+  elemekElerese2();
+  elemekElerese3();
+  elemekElerese4();
+  elemekFormazasa1();
+  esemenykezeles1();
+  esemenykezeles2();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+  const ELEM = document.querySelectorAll("#f1")[0];
+  console.log(ELEM.innerHTML);
 }
+
 function elemekElerese2() {
-    /**Ide jön az 2.  feladat */
+  const ELEM = document.querySelectorAll("#ide")[0];
+  ELEM.innerHTML = "<p>Jó reggelt!</p>";
 }
+
 function elemekElerese3() {
-    /**Ide jön az 3. feladat */
+  ELEM = document.querySelectorAll(".ide")[0];
+  ELEM.innerHTML = "<p>Jó reggelt!</p>";
 }
+
+function elemekElerese4() {
+  let txt = "<ul>";
+  for (let index = 0; index < 5; index++) {
+    txt += `<li> ${Math.floor(Math.random() * 20 + 10)} </li>`;
+  }
+  txt += "</ul>";
+
+  ELEM = document.getElementsByClassName("lista")[0];
+  ELEM.innerHTML = txt;
+}
+
+function elemekFormazasa1() {
+  ELEM = document.querySelectorAll(".lista")[0];
+  ELEM.classList.add("formazott");
+}
+
+function esemenykezeles1() {
+  const buttonELEM = document.querySelectorAll(".lista")[0];
+  console.log(buttonELEM);
+  buttonELEM.addEventListener("click", kattintas);
+}
+function kattintas() {
+    const buttonELEM = document.querySelectorAll(".lista")[0];
+    document.getElementsByClassName("kattintasutan")[0].innerHTML =
+    buttonELEM.innerHTML;
+}
+
+
+function esemenykezeles2() {
+    ELEM = document.getElementsByClassName("feladat")[0];
+    ELEM.innerHTML = "<button>OK</button>"
+    ELEM = document.getElementsByClassName("feladat button")[0];
+    const buttonELEM = document.querySelectorAll(".feladat button")[0];
+    console.log(buttonELEM);
+    buttonELEM.addEventListener("click", kattintas2);
+  }
+  function kattintas2() {
+    const buttonELEM = document.querySelectorAll(".feladat")[0];
+    document.getElementsByClassName("feladat")[0].innerHTML +=
+    buttonELEM.innerHTML = "<div><img src='unnamed.gif' alt='tuta'></div>";
+}
+
+function esemenykezeles3() {
+    
+  }
